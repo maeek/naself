bceda3c90eea319341e8e8d06ac335e4a49e0bc9
maek <maciej@suchanecki.me>
2023-12-10 22:39:10 +0100

    [common] feat: add divider to spacer component

--- production/web-client/src/components/common/spacer/spacer.tsx
+++ production/web-client/src/components/common/spacer/spacer.tsx
@@ -2,16 +2,25 @@ import classNames from 'classnames';
 import './spacer.scss';
 
 export interface SpacerProps {
-  size?: 'small' | 'medium' | 'large';
+  size?: 'extra-small' | 'small' | 'medium' | 'large';
   type?: 'horizontal' | 'vertical';
+  withDivider?: boolean;
   className?: string;
 }
 
-export const Spacer = ({ size = 'medium', type = 'vertical', className }: SpacerProps) => {
+export const Spacer = ({ size = 'medium', type = 'vertical', withDivider, className }: SpacerProps) => {
   return (
     <div
       data-testid='spacer'
-      className={classNames('spacer', `spacer--${size}`, `spacer--${type}`, className)}
+      className={classNames(
+        'spacer',
+        `spacer--${size}`,
+        `spacer--${type}`,
+        {
+          'spacer--with-divider': withDivider
+        },
+        className
+      )}
     />
   );
 };
