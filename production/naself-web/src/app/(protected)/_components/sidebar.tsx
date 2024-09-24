c89759818fadb3fc8e0536892d24b3bc8a108b81
maek <39914671+maeek@users.noreply.github.com>
2024-09-24 19:34:37 +0200

    feat: sidebar for mobile & style fixes

--- production/naself-web/src/app/(protected)/_components/sidebar.tsx
+++ production/naself-web/src/app/(protected)/_components/sidebar.tsx
@@ -20,7 +20,13 @@ import {
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-export default function Sidebar() {
+export default function Sidebar({
+  forceMobile,
+  closeNodeSlot
+}: {
+  forceMobile?: boolean
+  closeNodeSlot?: React.ReactNode
+}) {
   const pathname = usePathname()
 
   const filesSections = [
@@ -43,14 +49,17 @@ export default function Sidebar() {
 
   const appsSections = [
     { name: 'Clipboard', Icon: ClipboardIcon, href: '/tools/clipboard' },
-    { name: 'Converter', Icon: FolderSyncIcon },
-    { name: 'Downloader', Icon: HardDriveDownloadIcon }
+    { name: 'Downloader', Icon: HardDriveDownloadIcon, href: '/tools/downloader' },
+    { name: 'Converter', Icon: FolderSyncIcon }
   ]
 
   return (
-    <aside className='h-full w-72 border-r border-r-layout-border flex flex-col bg-layout overflow-auto pb-8 fixed left-0 top-16 max-lg:hidden'>
+    <aside
+      className={`h-full w-72 border-r border-r-layout-border flex flex-col bg-layout overflow-auto pb-8 fixed left-0 top-16 ${forceMobile ? 'max-lg:top-0 max-lg:relative max-lg:w-full max-lg:border-0' : 'max-lg:hidden'}`}
+    >
+      {closeNodeSlot}
       <div className='w-full py-6 flex flex-col px-2'>
-        <h6 className='text-xs pb-2 font-semibold color-secondary px-2'>Files</h6>
+        <h6 className='text-xs pb-2 font-semibold color-secondary px-2 max-lg:text-sm'>Files</h6>
         <ul className='w-full pb-6 flex flex-col'>
           {filesSections.map(({ name, Icon, href }) => (
             <Link
@@ -59,19 +68,19 @@ export default function Sidebar() {
             >
               <li
                 className={classNames(
-                  'flex gap-3 justify-start items-center px-2 py-1 hover:bg-input rounded-sm text-foreground hover:text-white cursor-pointer',
+                  'flex gap-3 justify-start items-center px-2 py-1 max-lg:py-2 hover:bg-input rounded-sm text-foreground hover:text-white cursor-pointer',
                   { 'bg-input': pathname === href }
                 )}
               >
                 <div className='w-6 h-6 rounded-sm flex justify-center items-center'>
-                  <Icon className='w-4 h-4 stroke-current' />
+                  <Icon className='w-4 h-4 max-lg:w-5 max-lg:h-5 stroke-current' />
                 </div>
-                <span className='text-sm text-inherit'>{name}</span>
+                <span className='text-sm max-lg:text-lg text-inherit'>{name}</span>
               </li>
             </Link>
           ))}
         </ul>
-        <h6 className='text-xs pb-2 font-semibold color-secondary px-2'>Settings</h6>
+        <h6 className='text-xs pb-2 font-semibold color-secondary px-2 max-lg:text-sm'>Settings</h6>
         <ul className='w-full pb-6 flex flex-col'>
           {settingsSections.map(({ name, Icon, href }) => (
             <Link
@@ -80,19 +89,19 @@ export default function Sidebar() {
             >
               <li
                 className={classNames(
-                  'flex gap-3 justify-start items-center px-2 py-1 hover:bg-input rounded-sm text-foreground hover:text-white cursor-pointer',
+                  'flex gap-3 justify-start items-center px-2 py-1 max-lg:py-2 hover:bg-input rounded-sm text-foreground hover:text-white cursor-pointer',
                   { 'bg-input': pathname === href }
                 )}
               >
                 <div className='w-6 h-6 rounded-sm flex justify-center items-center'>
-                  <Icon className='w-4 h-4 stroke-current' />
+                  <Icon className='w-4 h-4 max-lg:w-5 max-lg:h-5 stroke-current' />
                 </div>
-                <span className='text-sm text-inherit'>{name}</span>
+                <span className='text-sm max-lg:text-lg text-inherit'>{name}</span>
               </li>
             </Link>
           ))}
         </ul>
-        <h6 className='text-xs pb-2 font-semibold color-secondary px-2'>Tools</h6>
+        <h6 className='text-xs pb-2 font-semibold color-secondary px-2 max-lg:text-sm'>Tools</h6>
         <ul className='w-full pb-6 flex flex-col'>
           {appsSections.map(({ name, Icon, href }) => (
             <Link
@@ -101,14 +110,14 @@ export default function Sidebar() {
             >
               <li
                 className={classNames(
-                  'flex gap-3 justify-start items-center px-2 py-1 hover:bg-input rounded-sm text-foreground hover:text-white cursor-pointer',
+                  'flex gap-3 justify-start items-center px-2 py-1 max-lg:py-2 hover:bg-input rounded-sm text-foreground hover:text-white cursor-pointer',
                   { 'bg-input': pathname === href }
                 )}
               >
                 <div className='w-6 h-6 rounded-sm flex justify-center items-center'>
-                  <Icon className='w-4 h-4 stroke-current' />
+                  <Icon className='w-4 h-4 max-lg:w-5 max-lg:h-5 stroke-current' />
                 </div>
-                <span className='text-sm text-inherit'>{name}</span>
+                <span className='text-sm max-lg:text-lg text-inherit'>{name}</span>
               </li>
             </Link>
           ))}
--- production/naself-web/src/app/(protected)/_components/top-bar.tsx
+++ production/naself-web/src/app/(protected)/_components/top-bar.tsx
@@ -1,6 +1,6 @@
 import { CSSProperties } from 'react'
 import classNames from 'classnames'
-import { LogOutIcon, SettingsIcon, UserIcon } from 'lucide-react'
+import { LogOutIcon, MenuIcon, SettingsIcon, SidebarCloseIcon, UserIcon } from 'lucide-react'
 import Link from 'next/link'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
@@ -13,13 +13,15 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Input } from '@/components/ui/input'
 import { NaselfLogo } from '@/components/ui/naself-logo'
+import { Sheet, SheetContent, SheetClose, SheetTrigger } from '@/components/ui/sheet'
 import { ThemeToggle } from '@/components/ui/theme-toggle'
+import Sidebar from './sidebar'
 
 export const TopBarProtected = () => {
   const sectionClasses = 'flex items-center h-full px-4 gap-x-4 max-sm:px-2 sm:w-[33%]'
 
   return (
-    <div className='z-50 flex justify-between h-16 w-dvw border-b border-b-layout-border bg-layout flex-shrink-0 sticky top-0'>
+    <div className='z-50 flex justify-between h-16 w-dvw border-b border-b-layout-border bg-layout flex-shrink-0 sticky top-0 max-w-full'>
       <div className={classNames(sectionClasses, 'justify-start max-sm:hidden')}>
         <Link href='/'>
           <NaselfLogo />
@@ -28,6 +30,32 @@ export const TopBarProtected = () => {
       <div
         className={classNames(sectionClasses, 'justify-center flex-initial w-[26rem] max-sm:w-full max-w-screen-sm')}
       >
+        <div className='flex items-center lg:hidden'>
+          <Sheet>
+            <SheetTrigger>
+              <MenuIcon className='w-6 h-6 ml-2' />
+            </SheetTrigger>
+            <SheetContent
+              side='left'
+              className='p-0 w-full'
+            >
+              <Sidebar
+                forceMobile
+                closeNodeSlot={
+                  <div className='w-full p-4 sticky top-0 bg-layout border-b flex justify-between items-center'>
+                    <SheetClose className='flex gap-2 items-center'>
+                      <SidebarCloseIcon className='w-6 h-6' /> Close
+                    </SheetClose>
+                    <Avatar>
+                      <AvatarImage src='https://static.suchanecki.me/avatar.png' />
+                      <AvatarFallback>MA</AvatarFallback>
+                    </Avatar>
+                  </div>
+                }
+              />
+            </SheetContent>
+          </Sheet>
+        </div>
         <Input
           placeholder='Search files or execute an action'
           type='search'
