ccf969773b27163a5fe703e511e1733b6ddd9fec
maek <39914671+maeek@users.noreply.github.com>
2024-09-29 19:46:30 +0200

    style: fix layout

--- production/naself-web/src/app/(protected)/layout.tsx
+++ production/naself-web/src/app/(protected)/layout.tsx
@@ -5,9 +5,9 @@ export default function FilesLayout({ children }: { children: React.ReactNode })
   return (
     <div className='main-layout'>
       <TopBarProtected />
-      <div className='h-[calc(100dvh-4rem)] w-full'>
+      <div className='h-[calc(100%-4rem)] w-full'>
         <Sidebar />
-        <div className='pl-72 max-lg:pl-0 h-full'>{children}</div>
+        <div className='pl-72 max-lg:pl-0 min-h-full'>{children}</div>
       </div>
     </div>
   )
